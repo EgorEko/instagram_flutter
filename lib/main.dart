52f3e814ef3cd3c711c5ce8a3f31f44b42245589
egoreko <asya.wear@gmail.com>
2022-03-29 10:49:43 +0300

    Set Firebase Login Auth funtionality

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:instagram_flutter/screens/signup_screen.dart';
+import 'package:instagram_flutter/screens/login_screen.dart';
 import 'package:instagram_flutter/utils/colors.dart';
 
 void main() async {
@@ -34,7 +34,7 @@ class MyApp extends StatelessWidget {
       /*home: const ResponsiveLayout(
           mobileScreenLayout: MobileScreenLayout(),
           webScreenLayout: WebScreenLayout()),*/
-      home: const SignupScreen(),
+      home: const LoginScreen(),
     );
   }
 }
